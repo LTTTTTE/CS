40805993c4a41c98169e88c3143955e55efd7e28
LTTTTTE <10100aaaaa@gmail.com>
2018-10-01 11:29:38 +0900

    7.0추가된것 튜플,노람다

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+namespace CS7._0_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arg = Console.ReadLine();
+
+            var (sum, count) = T_F(Strings.Split(arg, " "));
+            Console.WriteLine($"Sum : {sum},Count : {count}");
+
+
+            (int sum, int count) T_F(string[] values)
+            {
+                var r = (s:0,c:0);
+                foreach(var v in values)
+                {
+                    /* r.s += int.Parse(v);
+                     r.c++;*/
+                    add(int.Parse(v), 1);
+                }
+
+
+                void add(int s, int c) { r.s += s; r.c += c; }
+
+                return r;
+            }
+        }
+    }
+}
